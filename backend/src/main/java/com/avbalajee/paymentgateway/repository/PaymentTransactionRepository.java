7303712b074acb81b15502b783ddeb3ff1b2c5dc
AVBalajee <balajeevg@gmail.com>
2026-03-17 18:43:31 +0530

    Adding JPA repository for payment transactions

---
+++ backend/src/main/java/com/avbalajee/paymentgateway/repository/PaymentTransactionRepository.java
@@ -0,0 +1,12 @@
+package com.avbalajee.paymentgateway.repository;
+
+import com.avbalajee.paymentgateway.entity.PaymentTransaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PaymentTransactionRepository extends JpaRepository<PaymentTransaction, String> {
+    Optional<PaymentTransaction> findByIdempotencyKey(String idempotencyKey);
+    List<PaymentTransaction> findTop20ByOrderByCreatedAtDesc();
+}
